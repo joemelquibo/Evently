2cdbf6976296540acf22d0ba62d867f2a77f0087
joemelquibo <joemelquibo@gmail.com>
2026-04-21 13:10:12 +0800

    INITIAL COMMIT

---
+++ Evently/Evently/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Evently.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Evently/Evently/Models/Users.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Evently.Models
+{
+    public class Users
+    {
+        [Key]
+        public int User_Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+    }
+}
