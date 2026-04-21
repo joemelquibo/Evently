d6252707c5fd9eb901e4a9ccc40dd38f239065d7
joemelquibo <joemelquibo@gmail.com>
2026-04-21 18:03:04 +0800

    Controller Added

---
+++ Evently/Evently/Controllers/MainController.cs
@@ -0,0 +1,19 @@
+﻿using Evently.DB;
+using Microsoft.AspNetCore.Mvc;
+using Evently.Models;
+
+namespace Evently.Controllers
+{
+    public class MainController : Controller
+    {
+        private readonly AppDbContext _context;
+        public MainController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
