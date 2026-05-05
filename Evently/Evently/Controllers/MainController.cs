ff8beb4f67f2803e6ad532df4aa4f6f718ce6ca4
Kien <kien.berdida@gmail.com>
2026-05-05 15:25:47 +0800

    1Added MainControllers.cs, this is just temporary

---
+++ Evently/Evently/Controllers/MainController.cs
@@ -0,0 +1,42 @@
+﻿using Evently.DB;
+using Microsoft.AspNetCore.Mvc;
+using Evently.Models;
+
+namespace Evently.Controllers
+{
+    public class MainController : Controller
+    {
+        private readonly AppDbContext _context;
+        public MainController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var events = _context.Events.ToList();
+            return View(events);
+        }
+    
+    
+        [HttpGet]
+        public IActionResult CreateEvent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateEvent(Events evt)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Events.Add(evt);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(evt);
+        }
+    }
+
+}
